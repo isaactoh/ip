503bba8d95a74abd4d958d515d33cfe19bc847a7
isaactoh <isaactoh29@gmail.com>
2025-08-20 13:13:52 +0800

    Level 1

--- src/main/java/ACow.java
+++ src/main/java/ACow.java
@@ -1,17 +1,24 @@
+import java.util.Scanner;
+
 public class ACow {
     public static void f(String command) {
         System.out.println("____________________________________________________________");
         if (command == "greet") {
             System.out.println("Hello! I'm acow123_bot");
             System.out.println("What can I do for you?");
-        } else if (command == "exit") {
-            System.out.println("Bye. Hope to see you again soon!");
+        } else {
+            System.out.println(command);
         }
         System.out.println("____________________________________________________________");
     }
 
     public static void main(String[] args) {
+        Scanner listener = new Scanner(System.in);
         f("greet");
-        f("exit");
+
+        while (true) {
+            String command = listener.nextLine();
+            f(command);
+        }
     }
 }
