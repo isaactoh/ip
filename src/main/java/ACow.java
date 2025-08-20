1aa07d6c85920bb438986c41f51de04276317814
isaactoh <isaactoh29@gmail.com>
2025-08-20 17:37:22 +0800

    Level 1 Corrected

--- src/main/java/ACow.java
+++ src/main/java/ACow.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
 
 public class ACow {
-    public static void f(String command) {
+    public static void greet() {
         System.out.println("____________________________________________________________");
-        if (command == "greet") {
-            System.out.println("Hello! I'm acow123_bot");
-            System.out.println("What can I do for you?");
-        } else {
-            System.out.println(command);
-        }
+        System.out.println("Hello! I'm acow123_bot");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void echo(String command) {
+        System.out.println("____________________________________________________________");
+        System.out.println(command);
         System.out.println("____________________________________________________________");
     }
 
     public static void main(String[] args) {
         Scanner listener = new Scanner(System.in);
-        f("greet");
+        greet();
 
         while (true) {
             String command = listener.nextLine();
-            f(command);
+            echo(command);
         }
     }
 }
