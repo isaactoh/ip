d4d7ad5cd8f4366db7f2d54baef85b5d6cb97ae7
isaactoh <isaactoh29@gmail.com>
2025-08-27 16:46:46 +0800

    Add date logic to Deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,23 +1,30 @@
-public class Deadline extends Task {
-    private String by;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String name, String by) {
-        super(name);
-        this.by = by;
+public class Deadline extends Task {
+    private LocalDate by;
+
+    private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy");
+
+    public Deadline(String description, String byStr) {
+        super(description);
+        this.by = LocalDate.parse(byStr, INPUT_FORMAT);
     }
 
-    public Deadline(String name, String by, boolean isDone) {
-        super(name,isDone);
-        this.by = by;
+    public Deadline(String description, String byStr, boolean isDone) {
+        super(description, isDone);
+        this.by = LocalDate.parse(byStr, INPUT_FORMAT);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(OUTPUT_FORMAT) + ")";
     }
 
     @Override
     public String store() {
-        return "D" + super.store() + " | " + by;
+        // store in yyyy-MM-dd format for consistency
+        return "D" + super.store() + " | " + by.format(INPUT_FORMAT);
     }
 }
