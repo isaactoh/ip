333ced30f3466521a33af6d7c0dfff90af576aa5
isaactoh <isaactoh29@gmail.com>
2025-08-27 17:34:02 +0800

    Separate UI class from ACow

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,4 +1,63 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Ui {
+    private Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void showGreeting() {
+        printDashes();
+        System.out.println("Hello! I'm acow123_bot");
+        System.out.println("What can I do for you?");
+        printDashes();
+    }
+
+    public void showAdios() {
+        System.out.println("Good night my pookies! Rest well and dream of victory in our");
+        System.out.println("next race. See you on the track tomorrow. -Adarsh");
+    }
+
+    public void showAdded(Task task, int size) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + task);
+        System.out.println("Now you have " + size + " tasks in this list.");
+    }
+
+    public void showList(ArrayList<Task> tasks) {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
+    }
+
+    public void showMarked(Task task) {
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  " + task);
+    }
+
+    public void showUnmarked(Task task) {
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("  " + task);
+    }
+
+    public void showDeleted(Task task, int size) {
+        System.out.println("Task removed like me from TREX soon:");
+        System.out.println("  " + task);
+        System.out.println("Now you have " + size + " tasks in the list.");
+    }
+
+    public void printDashes() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public void showError(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
+    public String readCommand() {
+        return scanner.nextLine();
+    }
 }
