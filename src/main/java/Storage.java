026cf05f85ebe5991b956fbbaaf36359c909ad84
isaactoh <isaactoh29@gmail.com>
2025-08-27 14:55:57 +0800

    Add storage logic

---
+++ src/main/java/Storage.java
@@ -0,0 +1,52 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Storage {
+    private final String filePath;
+
+    public Storage(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public ArrayList<Task> load() throws IOException {
+        ArrayList<Task> tasks = new ArrayList<>();
+        File file = new File(filePath);
+
+        if (!file.exists()) {
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+        }
+
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            tasks.add(TaskLoader.load(line));
+        }
+        scanner.close();
+        return tasks;
+    }
+
+    public void save(ArrayList<Task> tasks) throws IOException {
+        FileWriter writer = new FileWriter(filePath);
+        for (Task task : tasks) {
+            writer.write(task.store() + System.lineSeparator());
+        }
+        writer.close();
+    }
+
+    public static void main(String[] args) {
+        Storage storage = new Storage("data/acow.txt");
+        ArrayList<Task> tasks = null;
+        try {
+            tasks = storage.load();
+        } catch (IOException e) {
+            System.out.println("Hi");
+        }
+        for (Task t : tasks) {
+            System.out.println(t);
+        }
+    }
+}
